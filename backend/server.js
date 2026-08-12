16e16e50c8552503ada4512928b425d16a9f4524
Atuhe Kambonde <atuhekambonde@gmail.com>
2026-08-12 11:58:10 +0200

    Connect backend to MySQL database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const db = require('./config/db');
 
 const app = express();
 const PORT = 5000;
 
 app.get('/', (req, res) => {
-  res.send('Altus backend is running.');
+  res.send('Altus ICT backend is running.');
 });
+
+db.query('SELECT 1')
+  .then(() => console.log('Database connected successfully.'))
+  .catch((err) => console.error('Database connection failed:', err.message));
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
